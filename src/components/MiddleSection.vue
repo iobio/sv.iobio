4b4e636326af175a0d12aa190bf609cf84db5b1f
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-03-15 14:07:40 -0600

    Add right & middle Section

---
+++ src/components/MiddleSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="middle-section"></div>
+  </template>
+  
+  <script>
+  export default {
+  name: middleSection,
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  }
+  </script>
+  
+  <style lang="sass">
+  
+  </style>
---
+++ src/components/RightSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="right-section"></div>
+  </template>
+  
+  <script>
+  export default {
+  name: rightSection,
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  }
+  </script>
+  
+  <style lang="sass">
+  
+  </style>
