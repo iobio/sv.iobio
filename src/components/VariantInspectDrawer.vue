4d5b440a84ea9feb2b872051760c0b0f6b11408d
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-08-05 11:35:15 -0600

    Create VariantInspectDrawer.vue

---
+++ src/components/VariantInspectDrawer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="variant-inspect-drawer">
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  name: 'VariantInspectDrawer',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  }
+  </script>
+  
+  <style lang="sass">
+    #variant-inspect-drawer
+      width: 100%
+  </style>
