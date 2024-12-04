cd1e96e4dc06c4a999c07b5dccad32338a093032
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-12-04 14:09:02 -0700

    Update LeftTracksSection.vue

--- src/components/LeftTracksSection.vue
+++ src/components/LeftTracksSection.vue
@@ -128,6 +128,12 @@
             :geneCandidates="genesOfInterest"
             :chromosomeAccumulatedMap="chromosomeAccumulatedMap"
             @close="closeLowerModal"/>
+        <div class="show-modal-btn" @click="showLowerModal" v-if="focusedVariant && hideLowerModal">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <title>dots-horizontal</title>
+                <path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
+            </svg>
+        </div>
       </div>
 
     </div>
@@ -196,6 +202,9 @@
   methods: {
     closeLowerModal() {
       this.hideLowerModal = true;
+    },
+    showLowerModal() {
+      this.hideLowerModal = false;
     },
     toggleLineTool() {
       this.tools.line = !this.tools.line
@@ -684,6 +693,24 @@
                 fill: #2A65B7
                 border-radius: 50%
                 margin-left: 3px
+  .show-modal-btn
+    position: absolute
+    right: 5px
+    bottom: 10px
+    background-color: #2A65B7
+    border-radius: 50%
+    width: 30px
+    height: 30px
+    display: flex
+    justify-content: center
+    align-items: center
+    &:hover
+      cursor: pointer
+      background-color: #3778D2
+    svg
+      fill: white
+      width: 20px
+      height: 20px
   #radios-tools-container
     display: flex
     justify-content: flex-start
