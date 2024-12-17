1c202952ee97a23e21df818d729be384ee3b67b9
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-12-17 11:30:53 -0700

    Simplify gene card hpo title

--- src/components/parts/GeneAssociationsCard.vue
+++ src/components/parts/GeneAssociationsCard.vue
@@ -4,7 +4,7 @@
             <div class="symbol-row">{{ gene.gene_symbol }}</div> 
             <div class="header-row">
                 <div class="centered">
-                    Phenotypes <span class="blue">{{ numOverlappedByGene(gene) }}</span>/{{ patientPhenotypes.length }}
+                    Patient HPO <span class="blue">{{ numOverlappedByGene(gene) }}</span>
                     <svg class="patient-relevant" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <title>patient-phenotype</title>
                         <path d="M21.1,12.5L22.5,13.91L15.97,20.5L12.5,17L13.9,15.59L15.97,17.67L21.1,12.5M10,17L13,20H3V18C3,15.79 6.58,14 11,14L12.89,14.11L10,17M11,4A4,4 0 0,1 15,8A4,4 0 0,1 11,12A4,4 0 0,1 7,8A4,4 0 0,1 11,4Z" />
