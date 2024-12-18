d3a20eee243a1f121353e8a4e47bc09d0692f7b6
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-12-18 15:00:31 -0700

    Reformat data helpers

--- src/dataHelpers/dataHelpers.js
+++ src/dataHelpers/dataHelpers.js
@@ -1,82 +1,80 @@
-
 // const BACKEND_URL_BASE = 'http://localhost:7477'; //Development
-const BACKEND_URL_BASE = 'https://mosaic-staging.chpc.utah.edu/sv.iobio/backend'; //Production
+const BACKEND_URL_BASE = "https://mosaic-staging.chpc.utah.edu/sv.iobio/backend"; //Production
 
 //GET CROMOSOMES
-export async function getChromosomes(build='hg38') {
+export async function getChromosomes(build = "hg38") {
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/chromosomes?build=${build}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting Cromosomes:', error);
+        console.error("Error Getting Cromosomes:", error);
     }
 }
 
 //GET CENTROMERES
-export async function getCentromeres(build='hg38') {
+export async function getCentromeres(build = "hg38") {
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/centromeres?build=${build}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting Centromeres:', error);
+        console.error("Error Getting Centromeres:", error);
     }
 }
 
 //GET BANDS
-export async function getBands(build='hg38') {
+export async function getBands(build = "hg38") {
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/bands?build=${build}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting Bands', error);
+        console.error("Error Getting Bands", error);
     }
 }
 
 //GET GENES
-export async function getGenes(build='hg38', source='refseq') {
+export async function getGenes(build = "hg38", source = "refseq") {
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/genes?build=${build}&source=${source}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting Genes:', error);
+        console.error("Error Getting Genes:", error);
     }
 }
 
 //GET SVS FROM VCF
-export async function getSVsFromVCF(vcfFile, sampleName=null) {
+export async function getSVsFromVCF(vcfFile, sampleName = null) {
     try {
         if (!sampleName) {
             const response = await fetch(`${BACKEND_URL_BASE}/dataFromVcf?vcfPath=${vcfFile}`);
             const data = await response.json();
-            return data;            
+            return data;
         } else {
             const response = await fetch(`${BACKEND_URL_BASE}/dataFromVcf?vcfPath=${vcfFile}&sampleName=${sampleName}`);
             const data = await response.json();
             return data;
         }
-
     } catch (error) {
-        console.error('Error Getting VCF Data:', error);
+        console.error("Error Getting VCF Data:", error);
     }
 }
 
 //GET VCF SAMPLES FROM URL
-export async function getVCFSamplesFromURL(url, ) {
+export async function getVCFSamplesFromURL(url) {
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/vcfSamples?vcfPath=${url}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting VCF Samples:', error);
+        console.error("Error Getting VCF Samples:", error);
     }
 }
 
 //GET QUALITY FROM VCF
-export async function getQualityFromVCF(vcfFile, sampleName=null) {
+export async function getQualityFromVCF(vcfFile, sampleName = null) {
     try {
         if (!sampleName) {
             const response = await fetch(`${BACKEND_URL_BASE}/vcfQuality?vcfPath=${vcfFile}`);
@@ -88,27 +86,26 @@ export async function getQualityFromVCF(vcfFile, sampleName=null) {
             return data;
         }
     } catch (error) {
-        console.error('Error Getting VCF Quality:', error);
+        console.error("Error Getting VCF Quality:", error);
     }
 }
 
 //GET SV BATCH INFO
-export async function getSVBatchInfo(svBatch, build='hg38', source='refseq') {
-    const svBatchJson = JSON.stringify({variants: svBatch});
+export async function getSVBatchInfo(svBatch, build = "hg38", source = "refseq") {
+    const svBatchJson = JSON.stringify({ variants: svBatch });
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/sv/info/batch?source=${source}&build=${build}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: svBatchJson
+            body: svBatchJson,
         });
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting SV Batch Info:', error);
+        console.error("Error Getting SV Batch Info:", error);
     }
-
 }
 
 //GET GENES FOR PHENOTYPES
@@ -118,19 +115,18 @@ export async function getGenesForPhenotypes(phenotypes) {
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting Genes for Phenotypes:', error);
+        console.error("Error Getting Genes for Phenotypes:", error);
     }
-
 }
 
 //GET TRANSCRIPTS FOR GENE LIST
-export async function getTranscriptsForGenes(geneList, build='hg38', source='gencode') {
-    const geneListString = geneList.join(',');
+export async function getTranscriptsForGenes(geneList, build = "hg38", source = "gencode") {
+    const geneListString = geneList.join(",");
     try {
         const response = await fetch(`${BACKEND_URL_BASE}/transcripts?genes=${geneList}&build=${build}&source=${source}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error Getting Transcripts for Gene List:', error);
+        console.error("Error Getting Transcripts for Gene List:", error);
     }
 }
