dff3c97a193a14fc2b7b29d3937174faf139bba9
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-02-14 15:11:39 -0700

    Add template and variables

---
+++ src/d3/clinGenChart.d3.js
@@ -0,0 +1,8 @@
+import * as d3 from "d3";
+
+export default function clinGenChart(parentElementTag, clinGenRegions, refChromosomes, options) {
+    let parentElement = d3.select(parentElementTag);
+
+    let width = parentElement.node().clientWidth;
+    let height = parentElement.node().clientHeight;
+}
