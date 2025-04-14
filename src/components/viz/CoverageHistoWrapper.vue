36fd456218a68b85885b47aed414a55315db4fa8
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-04-14 13:21:18 -0600

    Remove manual update

--- src/components/viz/CoverageHistoWrapper.vue
+++ src/components/viz/CoverageHistoWrapper.vue
@@ -30,38 +30,6 @@ export default {
         // Append the elements to the container
         container.appendChild(this.dataBrokerEl);
         container.appendChild(this.coverageHisto);
-
-        // For now we are going to pick out the elements we dont want and hide them
-        let shadow = document.querySelector("iobio-coverage-depth").shadowRoot;
-
-        if (shadow) {
-            // const style = document.createElement("style");
-            // style.textContent = `
-            //     #bamview-controls {
-            //         display: none;
-            //         height: 0;
-            //     }
-            //     #title-container {
-            //         display: none;
-            //         height: 0;
-            //     }
-            //     #bamview-chart-container {
-            //         border: none;
-            //     }
-            //     .chromosome-button-small.chromosome-button {
-            //         display: none;
-            //     }
-            //     .circle-button-reset-chromosomes {
-            //         display: none;
-            //         height: 0;
-            //     }
-            //     .bar {
-            //         fill: #4d4d4d;
-            //         fill-opacity: 0.8;
-            //     }
-            // `;
-            // shadow.appendChild(style);
-        }
     },
     methods: {},
     computed: {},
