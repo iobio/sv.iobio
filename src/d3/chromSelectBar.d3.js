e8f61a5352d80b4be9e4c309fbfb6d972b2b91e4
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-05-19 10:48:57 -0600

    Remove plus btns

--- src/d3/chromSelectBar.d3.js
+++ src/d3/chromSelectBar.d3.js
@@ -220,52 +220,6 @@ export default function chromSelectBar(parentElementTag, refChromosomes, options
                 .attr("font-size", "14px")
                 .attr("font-weight", "bold")
                 .attr("fill", chromosomeColor);
-
-            //Add the zoom to button in the upper left corner
-            let btngroup = chromosomeGroup.append("g").attr("class", "zoom-to-button").attr("transform", `translate(0, -6)`);
-
-            btngroup
-                .append("svg")
-                .attr("xmlns", "http://www.w3.org/2000/svg")
-                .attr("viewBox", "0 0 24 24")
-                .attr("width", 15)
-                .attr("height", 15)
-                .append("path")
-                .attr(
-                    "d",
-                    "M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z"
-                )
-                .attr("title", "Zoom to chromosome")
-                .attr("fill", chromosomeColor)
-                .attr("stroke", chromosomeColor)
-                .attr("stroke-width", 0.25)
-                .attr("stroke-linecap", "round")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke-miterlimit", 10);
-
-            btngroup
-                .append("rect")
-                .attr("width", 15)
-                .attr("height", 15)
-                .attr("fill", "transparent")
-                .attr("cursor", "pointer")
-                .raise();
-
-            let zoombtn = chromosomeGroup.select(".zoom-to-button");
-            zoombtn.on("click", function () {
-                let path = d3.select(this).select("path");
-                path.attr("fill", "#174C8C").attr("stroke", "#174C8C").attr("transform", "scale(1.1)");
-
-                //set a timeout to call this next function
-                setTimeout(() => {
-                    let zoomedSelection = chromosomeMap.get(chr);
-                    if (zoomedSelection) {
-                        if (selectionCallback) {
-                            selectionCallback({ start: zoomedSelection.start, end: zoomedSelection.end });
-                        }
-                    }
-                }, 10);
-            });
         }
     }
 
