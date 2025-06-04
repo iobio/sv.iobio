f0bb3a1200af71751071b6dcb82761ba67249b3f
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-06-04 15:57:43 -0600

    Add custom element

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 7476,
-  },
-  plugins: [vue()],
-  base: '/sv.iobio/frontend/', //add a base path to the project
-})
+    server: {
+        port: 7476,
+    },
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => tag === "iobio-multi-series",
+                },
+            },
+        }),
+    ],
+    base: "/sv.iobio/frontend/", //add a base path to the project
+});
