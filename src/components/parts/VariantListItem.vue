a17ed1a3c0c1d653d4f041f3657e144020093543
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-07-03 11:36:38 -0600

    Add toggle on hide/unhide button

--- src/components/parts/VariantListItem.vue
+++ src/components/parts/VariantListItem.vue
@@ -19,11 +19,19 @@
                 </svg>
             </div>
 
-            <div class="hide-tag" @click.stop="hideVariant">
+            <div class="hide-tag" @click.stop="hideVariant" v-if="!isHidden">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <title>hide</title>
                     <path
                         d="M2,5.27L3.28,4L20,20.72L18.73,22L15.65,18.92C14.5,19.3 13.28,19.5 12,19.5C7,19.5 2.73,16.39 1,12C1.69,10.24 2.79,8.69 4.19,7.46L2,5.27M12,9A3,3 0 0,1 15,12C15,12.35 14.94,12.69 14.83,13L11,9.17C11.31,9.06 11.65,9 12,9M12,4.5C17,4.5 21.27,7.61 23,12C22.18,14.08 20.79,15.88 19,17.19L17.58,15.76C18.94,14.82 20.06,13.54 20.82,12C19.17,8.64 15.76,6.5 12,6.5C10.91,6.5 9.84,6.68 8.84,7L7.3,5.47C8.74,4.85 10.33,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C12.69,17.5 13.37,17.43 14,17.29L11.72,15C10.29,14.85 9.15,13.71 9,12.28L5.6,8.87C4.61,9.72 3.78,10.78 3.18,12Z" />
+                </svg>
+            </div>
+
+            <div class="hide-tag" @click.stop="hideVariant" v-if="isHidden">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <title>eye-plus-outline</title>
+                    <path
+                        d="M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C12.36,19.5 12.72,19.5 13.08,19.45C13.03,19.13 13,18.82 13,18.5C13,18.14 13.04,17.78 13.1,17.42C12.74,17.46 12.37,17.5 12,17.5C8.24,17.5 4.83,15.36 3.18,12C4.83,8.64 8.24,6.5 12,6.5C15.76,6.5 19.17,8.64 20.82,12C20.7,12.24 20.56,12.45 20.43,12.68C21.09,12.84 21.72,13.11 22.29,13.5C22.56,13 22.8,12.5 23,12C21.27,7.61 17,4.5 12,4.5M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M18,14.5V17.5H15V19.5H18V22.5H20V19.5H23V17.5H20V14.5H18Z" />
                 </svg>
             </div>
 
@@ -137,6 +145,7 @@ export default {
         overlapProp: Number,
         filters: Object,
         focusedVariant: Object,
+        isHidden: Boolean,
     },
     data() {
         return {
@@ -157,7 +166,11 @@ export default {
             this.$emit("favorite-variant", this.variant);
         },
         hideVariant() {
-            this.$emit("hide-variant", this.variant);
+            if (!this.isHidden) {
+                this.$emit("hide-variant", this.variant);
+            } else {
+                this.$emit("unhide-variant", this.variant);
+            }
         },
         variantOpened(event) {
             event.stopPropagation();
